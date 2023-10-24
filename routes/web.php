3f6c6a88b3f26c0ecf24538996901b4374c00150
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-24 11:48:01 -0300

    Aula 06 - Proteção de rotas com middlewares de autenticação

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', HomeController::class)->name('home');
-Route::get('/register',[RegisterController::class, 'create']);
-Route::post('/register',[RegisterController::class, 'store'])->name('register');
-Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/', HomeController::class)->name('home')->middleware('auth');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('/register',[RegisterController::class, 'create']);
+    Route::post('/register',[RegisterController::class, 'store'])->name('register');
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login.form');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
